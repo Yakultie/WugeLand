0f1c1dc7e350c4e5f5525c6368676a1600b625cc
hugo7171 <hugowong71@gmail.com>
2017-01-21 22:44:48 +0800

    BIG PUSH

--- personal/bernard_project/edgaretmarine.js
+++ personal/bernard_project/edgaretmarine.js
@@ -2,3 +2,14 @@ console.log("back off hackerssss");
 var money = 100;
 money_p = document.getElementById('money_p')
 money_p.innerHTML = money + " $"
+love = 0;
+hot = 0;
+torso= $("#bernard_torso")
+torso_width = $("#bernard_torso").width();
+function getFat() {
+
+	console.log(torso_width)
+	torso_width += 10;
+	console.log(torso_width)
+	document.getElementById("bernard_torso").style.width= torso_width+"px";
+}
